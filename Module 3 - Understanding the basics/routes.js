2e6c536878410660842e7aea4776b1a4f4cb0b2c
Ryan Kegerreis <Rkegerreis@assemblysoftware.com>
2019-12-27 10:15:51 -0500

    Finished module 3

--- Module 3 - Understanding the basics/routes.js
+++ Module 3 - Understanding the basics/routes.js
@@ -1,9 +1,9 @@
-const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((req, res) => {
+const requestHandler = (req, res) => {
 	const url = req.url;
 	const method = req.method;
+
 	if (url === '/') {
 		res.write('<html>');
 		res.write('<head><title>Enter Message</title></head>');
@@ -17,18 +17,18 @@ const server = http.createServer((req, res) => {
 	if (url === '/message' && method === 'POST') {
 		const body = [];
 		req.on('data', chunk => {
-			console.log(chunk);
 			body.push(chunk);
 		});
-		req.on('end', () => {
+		return req.on('end', () => {
 			const parsedBody = Buffer.concat(body).toString();
 			const message = parsedBody.split('=')[1];
 
-			fs.writeFile('message.txt', message);
+			fs.writeFile('message.txt', message, () => {
+				res.statusCode = 302;
+				res.setHeader('Location', '/');
+				return res.end();
+			});
 		});
-		res.statusCode = 302;
-		res.setHeader('Location', '/');
-		return res.end();
 	}
 
 	res.setHeader('Content-Type', 'text/html');
@@ -36,6 +36,7 @@ const server = http.createServer((req, res) => {
 	res.write('<head><title>My First Page</title></head>');
 	res.write('<body><h1>Hello from my Node Page</h1></body>');
 	res.write('</html>');
-});
+	res.end();
+};
 
-server.listen(3000);
+module.exports = requestHandler;
