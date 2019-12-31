778aa8c5a8accf103bfa5e27df20014c07ecdcb5
Ryan Kegerreis <Rkegerreis@assemblysoftware.com>
2019-12-31 09:38:45 -0500

    Express router

---
+++ module5-express/express-code-along/Routes/Admin.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const Router = express.Router();
+
+Router.get('/add-product', (req, res, next) => {
+	res.send(
+		'<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit" >Add Product</button></input></form>'
+	);
+});
+
+Router.post('/add-product', (req, res, next) => {
+	console.log(req.body);
+	res.redirect('/');
+});
+
+module.exports = Router;
--- module5-express/express-code-along/app.js
+++ module5-express/express-code-along/app.js
@@ -1,15 +1,20 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-	res.send('Add a product!');
-});
+const adminRoutes = require('./Routes/Admin');
+const shopRoutes = require('./Routes/Shop');
 
-app.use('/', (req, res, next) => {
-	res.send('Youre home!');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+	res.status(404).send('<h1>404 Page Not Found</h1>');
 });
 
 app.listen(3000);
